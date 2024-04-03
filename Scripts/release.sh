e720f45592b1ecbd6e286303379315498307c525
Ben Baron <bbaron@mparticle.com>
2024-04-03 10:36:34 -0500

    ci: Fix release script

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -5,11 +5,6 @@ NOTES="$2"
 # Update version number
 #
 
-# Update Carthage release json file
-jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mparticle-integrations/mparticle-apple-integration-adjust/releases/download/$PREFIXED_VERSION/mParticle_Adjust.framework.zip?alt=https://github.com/mparticle-integrations/mparticle-apple-integration-adjust/releases/download/$PREFIXED_VERSION/mParticle_Adjust.xcframework.zip"'"}'
-mParticle_Adjust.json > tmp.json
-mv tmp.json mParticle_Adjust.json
-
 # Update CocoaPods podspec file
 sed -i '' 's/\(^    s.version[^=]*= \).*/\1"'"$VERSION"'"/' mParticle-Adjust.podspec
 
@@ -17,8 +12,9 @@ sed -i '' 's/\(^    s.version[^=]*= \).*/\1"'"$VERSION"'"/' mParticle-Adjust.pod
 #
 
 git add mParticle-Adjust.podspec
-git add mParticle_Adjust.json
 git add CHANGELOG.md
 git commit -m "chore(release): $VERSION [skip ci]
 
 $NOTES"
+
+echo "done"
